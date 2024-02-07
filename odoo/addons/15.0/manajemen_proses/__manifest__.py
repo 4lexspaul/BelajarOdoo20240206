a7be5686a7db9d3847a9e8f12e41f90a774302ff
Alex <alexspaul24@gmail.com>
2024-02-07 17:10:17 +0700

    test commit

---
+++ odoo/addons/15.0/manajemen_proses/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Manajemen Proses',
+    'version': '1.0.0',
+    'category': 'Manajemen',
+    'author': 'Odoo PS',
+    'sequence': -100,
+    'summary': 'Manajemen Proses Sistem',
+    'description': """Manajemen Proses Sistem""",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/manpros_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3'
+}
---
+++ odoo/addons/15.0/manajemen_proses/models/patient.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models
+
+class ManajemenProses(models.Model):
+    _name = "manajemen.proses"
+    _description = "Manajemen Proses"
+
+    name = fields.Char(string='Name')
+    ref = fields.Char(string='References')
+    age = fields.Integer(String='Age')
+    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
+    type = fields.Char(string='Type')
+
+    name = fields.Char(string='Account Type', required=True, translate=True)
+#    include_initial_balance = fields.Boolean(string="Bring Accounts Balance Forward", help="Used in reports to know if we should consider journal items from the beginning of time instead of from the fiscal year only. Account types that should be reset to zero at each new fiscal year (like expenses, revenue..) should not have this option set.")
+#    type = fields.Selection([
+#        ('other', 'Regular'),
+#        ('receivable', 'Receivable'),
+#        ('payable', 'Payable'),
+#        ('liquidity', 'Liquidity'),
+#    ], required=True, default='other',
+#        help="The 'Internal Type' is used for features available on "\
+#        "different types of accounts: liquidity type is for cash or bank accounts"\
+#        ", payable/receivable is for vendor/customer accounts.")
