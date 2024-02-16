075b98c27965c97a55d35debf7d6b4a46d1917dd
Alex <alexspaul24@gmail.com>
2024-02-16 16:59:04 +0700

    1. Define Onchange Functions 2. Rec Name for Models 3. Add Notebook and Pages View 4. Define HTML field

--- odoo/addons/15.0/manajemen_proses/models/appointment.py
+++ odoo/addons/15.0/manajemen_proses/models/appointment.py
@@ -4,8 +4,16 @@ class ManajemenAppointment(models.Model):
     _name = "manajemen.appointment"
     _description = "Manajemen Appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'manajemen_id'
 
     manajemen_id = fields.Many2one(comodel_name='manajemen.proses', string="Manajemen")
     type = fields.Selection([('high', 'High'), ('middle', 'Middle'), ('low', 'Low')], string='Type', related='manajemen_id.type')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='References')
+    komentar = fields.Html(string='Komentar', translate=True)
+
+    @api.onchange('manajemen_id')
+    def _onchange_manajemen_id(self):
+        if self.manajemen_id:
+            self.ref = self.manajemen_id.ref
--- odoo/addons/15.0/manajemen_proses/models/patient.py
+++ odoo/addons/15.0/manajemen_proses/models/patient.py
@@ -8,12 +8,13 @@ class ManajemenProses(models.Model):
 
     name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='References', tracking=True)
-    age = fields.Integer(String='Age', compute='_compute_age', tracking=True)
+    age = fields.Integer(String='Age', compute='_compute_age', tracking=True, store=True)
     metode_manpros = fields.Selection([('kuantitatif', 'Kuantitatif'), ('kualitatif', 'Kualitatif')], string="Metode", tracking=True, default='kuantitatif')
     metode_analisis = fields.Selection([('common size financial statement','Common Size Financial Statement'),('index time series','Index Time Series'),('analisis pertumbuhan','Analisis Pertumbuhan'),('analisis industri','Analisis Industri')], string="Metode Analisis", tracking=True)
     type = fields.Selection([('high','High'), ('middle','Middle'), ('low','Low')], string='Type', tracking=True)
     active = fields.Boolean(string='Active', default=True, tracking=True)
     date_of_birth = fields.Date(string='Date of Birth', tracking=True)
+    appointment_id = fields.Many2one('manajemen.appointment', string="Appointment")
 
 
     @api.depends('date_of_birth')
